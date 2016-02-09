d9bb66333823ff435cc859c63a8d765348d31b55
Rakesh Yarlagadda <ry2294@columbia.edu>
2016-02-09 21:17:59 +0000

    Server setup complete

--- assignment-1/server.js
+++ assignment-1/server.js
@@ -1,5 +1,27 @@
+var express = require('express');
+var app = express();
 var Twitter = require('twitter');
-var _ = require('underscore');
+var path    = require("path");
+
+var router = express.Router();
+router.get('/', function(req, res) {
+  res.sendfile(path.join(__dirname + '/home.html'));
+});
+
+app.use('/', router);
+
+var http = require('http').Server(app);
+var server = http.listen(process.env.PORT || 5000, function(){
+  console.log('App listening at http://%s:%s', server.address().address, server.address().port);
+});
+
+var io = require('socket.io').listen(server);
+io.on('connection', function(socket){
+  console.log('user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 
 var client = new Twitter({
     consumer_key: "23wysyPCMUjcJCuUTX9HKDIVK",
@@ -16,9 +38,9 @@ client.stream('statuses/filter', {'locations':'-180,-90,180,90'},function(stream
             tweet.username = data.user.name;
             tweet.text = data.text;
             tweet.geo = data.geo;
-            //tweet.hashtags = data.entities.hashtags;
             tweet.source = data.source;
             console.log(tweet);
+            io.emit('tweet', JSON.stringify(tweet));
         }
     });
 });
